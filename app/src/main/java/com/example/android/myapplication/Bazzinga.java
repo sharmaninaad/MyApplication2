ac40cbb4ee0c1ae40ca7bdabdc1fa522fdd0c24c
ninaad <ninaad61348@gmail.com>
2018-10-07 13:24:04 +0530

    making category interactive

--- app/src/main/java/com/example/android/myapplication/Bazzinga.java
+++ app/src/main/java/com/example/android/myapplication/Bazzinga.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 import java.util.Arrays;
 
 public class Bazzinga extends AppCompatActivity {
-    Itemclass[] items_bazzinga = {
+    Itemclass[] items_bazzinga_smoothies = {
             new Itemclass("Cupcake", 15.0),
             new Itemclass("Donut", 16.0 ),
             new Itemclass("Eclair", 20.0),
@@ -20,9 +20,15 @@ public class Bazzinga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bazzinga);
-
-        MenuAdapter menuAdapter=new MenuAdapter(this,0, Arrays.asList(items_bazzinga));
+        MenuAdapter menuAdapter;
         ListView listView=findViewById(R.id.bazzingaMenu);
-        listView.setAdapter(menuAdapter);
+        int index=getIntent().getIntExtra("tag",0);
+        switch(index) {
+            case 2: menuAdapter = new MenuAdapter(this, 0, Arrays.asList(items_bazzinga_smoothies));
+                listView.setAdapter(menuAdapter);
+                break;
+        }
+       
+
     }
 }
