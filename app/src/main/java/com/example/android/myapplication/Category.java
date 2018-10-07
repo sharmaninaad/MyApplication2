c0fc920f104ef12ebd6130d073e8170e49dfc55e
ninaad <ninaad61348@gmail.com>
2018-10-07 12:36:12 +0530

    adding a category adapter

---
+++ app/src/main/java/com/example/android/myapplication/Category.java
@@ -0,0 +1,11 @@
+package com.example.android.myapplication;
+
+public class Category {
+    String Name;
+    int image;
+
+    public Category(String name, int image) {
+        Name = name;
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/android/myapplication/CategoryAdapter.java
+++ app/src/main/java/com/example/android/myapplication/CategoryAdapter.java
@@ -1,36 +1,32 @@
 package com.example.android.myapplication;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
-
-public class MenuAdapter extends ArrayAdapter {
-    public MenuAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+public class CategoryAdapter extends ArrayAdapter {
+    public CategoryAdapter(@NonNull Context context, int resource, @NonNull Object[] objects) {
         super(context, 0, objects);
     }
-    @NonNull
-    @SuppressLint("SetTextI18n")
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        Itemclass itemclass = (Itemclass) getItem(position);
-
+        Category category = (Category) getItem(position);
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_single, parent, false);
         }
 
-            TextView NameView =  convertView.findViewById(R.id.nameText);
-        assert itemclass != null;
-        NameView.setText(itemclass.Name);
 
-        TextView NumberView =  convertView.findViewById(R.id.priceText);
-        NumberView.setText(itemclass.price+" ");
+        TextView NameView =  convertView.findViewById(R.id.nameCategory);
+        assert category != null;
+        NameView.setText(category.Name);
+
+        ImageView imageView = convertView.findViewById(R.id.imageCategory);
+        imageView.setImageResource(category.image);
         return convertView;
     }
 }
